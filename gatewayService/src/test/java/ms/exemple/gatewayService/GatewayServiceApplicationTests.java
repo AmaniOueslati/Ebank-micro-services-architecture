02962c82e13faffaa45f202885b37068e0f19c2f
Amani <weslatiamani86@gmail.com>
2024-03-12 01:22:22 +0100

    ajout du gatway , discobery nd config services vides with dependencies

---
+++ gatewayService/src/test/java/ms/exemple/gatewayService/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ms.exemple.gatewayService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
